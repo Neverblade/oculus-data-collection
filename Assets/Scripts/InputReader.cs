680020b561bcbc4295340a2384290610822f83f1
James Lin <james97lin@gmail.com>
2019-03-11 22:10:45 -0700

    Added files

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class InputReader : MonoBehaviour {
+
+    public Transform cameraRig;
+
+    bool recording = false;
+    float startingTime;
+    int frames;
+    Transform headset;
+    Transform leftController;
+    Transform rightController;
+    StreamWriter sw;
+
+	// Use this for initialization
+	void Start () {
+        headset = cameraRig.Find("TrackingSpace").Find("CenterEyeAnchor");
+        leftController = cameraRig.Find("TrackingSpace").Find("LeftHandAnchor");
+        rightController = cameraRig.Find("TrackingSpace").Find("RightHandAnchor");
+
+        string dirPath = Application.persistentDataPath + "/exports";
+        Directory.CreateDirectory(dirPath);
+    }
+
+    private void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            recording = !recording;
+            if (recording) {
+                startingTime = Time.time;
+                frames = 0;
+                print("Creating temporary file.");
+                sw = new StreamWriter(Application.persistentDataPath + "/temp");
+                sw.WriteLine("Frame Time: " + Time.fixedDeltaTime);
+                print("Starting recording.");
+            } else {
+                sw.Close();
+                print("Closing temporary file.");
+                string fileName = DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss");
+                print("Creating export file: " + fileName);
+                StreamReader sr = new StreamReader(Application.persistentDataPath + "/temp");
+                sw = new StreamWriter(Application.persistentDataPath + "/exports/" + fileName + ".txt");
+                sw.WriteLine("Frames: " + frames);
+
+                string line;
+                while ((line = sr.ReadLine()) != null) {
+                    sw.WriteLine(line);
+                }
+
+                sr.Close();
+                sw.Close();
+                print("Finished recording.");
+            }
+        }
+
+        if (recording) {
+            sw.WriteLine(headset.position.x + " "
+                    + headset.position.y + " "
+                    + headset.position.z + " "
+                    + headset.eulerAngles.x + " "
+                    + headset.eulerAngles.y + " "
+                    + headset.eulerAngles.z + " "
+                    + leftController.position.x + " "
+                    + leftController.position.y + " "
+                    + leftController.position.z + " "
+                    + leftController.eulerAngles.x + " "
+                    + leftController.eulerAngles.y + " "
+                    + leftController.eulerAngles.z + " "
+                    + rightController.position.x + " "
+                    + rightController.position.y + " "
+                    + rightController.position.z + " "
+                    + rightController.eulerAngles.x + " "
+                    + rightController.eulerAngles.y + " "
+                    + rightController.eulerAngles.z);
+
+            frames++;
+
+            //print("Headset: " + headset.position);
+            //print("Left: " + leftController.position);
+            //print("Right: " + rightController.position);
+            //print("Time: " + (Time.time - startingTime));
+            //print("vs Time: " + Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/SpectatorCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectatorCamera : MonoBehaviour {
+
+    public float height = 5;
+    public float radius = 5;
+    public float speed = 10; // Degrees per second
+
+    private float currentDegrees = 0;
+
+	private void Start () {
+        transform.position = GetPositionOnCircle(0);
+	}
+	
+	private void Update () {
+        transform.position = GetPositionOnCircle(currentDegrees);
+        transform.LookAt(Vector3.zero, Vector3.up);
+        currentDegrees = (currentDegrees + speed * Time.deltaTime) % 360;
+	}
+
+    private Vector3 GetPositionOnCircle(float degrees) {
+        float radians = degrees * Mathf.PI / 180;
+        float x = radius * Mathf.Cos(radians);
+        float z = radius * Mathf.Sin(radians);
+        return new Vector3(x, height, z);
+    }
+}
